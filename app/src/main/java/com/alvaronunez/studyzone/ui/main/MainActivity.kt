dbea150ceb189766426d6fb874c88f6ee10d2728
Álvaro Núñez Hermida <alvaro.nunezhermida@plexus.es>
2019-12-11 21:14:48 +0100

    Implemented MVVM in main activity

--- app/src/main/java/com/alvaronunez/studyzone/ui/main/MainActivity.kt
+++ app/src/main/java/com/alvaronunez/studyzone/ui/main/MainActivity.kt
@@ -6,25 +6,28 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.alvaronunez.studyzone.R
-import com.alvaronunez.studyzone.data.model.ItemDTO
+import com.alvaronunez.studyzone.ui.main.MainViewModel.UiModel
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), MainPresenter.View {
+class MainActivity : AppCompatActivity(){
 
-    private val presenter by lazy { MainPresenter() }
-    private val adapter = ItemsAdapter(presenter::onItemClicked)
+    private lateinit var viewModel: MainViewModel
+    private lateinit var adapter : ItemsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        presenter.onCreate(this)
-        recycler.adapter = adapter
-    }
 
-    override fun onDestroy() {
-        presenter.onDestroy()
-        super.onDestroy()
+        viewModel = ViewModelProviders.of(
+            this,
+            MainViewModelFactory())[MainViewModel::class.java]
+
+        adapter = ItemsAdapter(viewModel::onItemClicked)
+        recycler.adapter = adapter
+        viewModel.model.observe(this, Observer(::updateUi))
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -34,28 +37,17 @@ class MainActivity : AppCompatActivity(), MainPresenter.View {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
-            R.id.menu_sign_up -> presenter.onSignOutClicked()
+            R.id.menu_sign_up -> viewModel.onSignOutClicked()
         }
         return false
     }
 
-    override fun finishActivity() {
-        finish()
-    }
-
-    override fun showMessage(message: String) {
-        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
-    }
-
-    override fun showProgress() {
-        loading.visibility = View.VISIBLE
-    }
-
-    override fun hideProgress() {
-        loading.visibility = View.GONE
-    }
-
-    override fun updateData(items: List<ItemDTO>) {
-        adapter.items = items
+    private fun updateUi(model: UiModel) {
+        loading.visibility = if (model is UiModel.Loading) View.VISIBLE else View.GONE
+        when (model) {
+            is UiModel.Content -> adapter.items = model.items
+            is UiModel.Message -> Toast.makeText(this, model.message, Toast.LENGTH_LONG).show()
+            is UiModel.Finish -> finish()
+        }
     }
 }
