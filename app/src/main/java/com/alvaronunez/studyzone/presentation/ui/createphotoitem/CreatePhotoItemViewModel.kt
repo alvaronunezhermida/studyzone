28aa58318009cffa6f5db7c857a8522ae959120e
Álvaro Núñez Hermida <alvaro.nunezhermida@plexus.es>
2020-01-30 13:28:28 +0100

    Start add photo item feature

---
+++ app/src/main/java/com/alvaronunez/studyzone/presentation/ui/createphotoitem/CreatePhotoItemViewModel.kt
@@ -0,0 +1,8 @@
+package com.alvaronunez.studyzone.presentation.ui.createphotoitem
+
+import com.alvaronunez.studyzone.presentation.ui.common.ScopedViewModel
+
+
+class CreatePhotoItemViewModel : ScopedViewModel() {
+
+}
