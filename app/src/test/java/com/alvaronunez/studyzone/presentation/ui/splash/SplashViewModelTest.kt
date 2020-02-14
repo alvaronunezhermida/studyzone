8b772fbecbb7f88306fcf26e5be91a271eb36316
Mikel Balduciel <mibaldi2@gmail.com>
2020-02-14 20:43:59 +0100

    fix test handle post delayed

--- app/src/test/java/com/alvaronunez/studyzone/presentation/ui/splash/SplashViewModelTest.kt
+++ app/src/test/java/com/alvaronunez/studyzone/presentation/ui/splash/SplashViewModelTest.kt
@@ -4,10 +4,7 @@ import android.os.Handler
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
 import com.alvaronunez.studyzone.domain.User
-import com.alvaronunez.studyzone.presentation.ui.common.Event
 import com.alvaronunez.studyzone.usecases.GetSignedUser
-import com.nhaarman.mockitokotlin2.any
-import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.whenever
 import kotlinx.coroutines.Dispatchers
@@ -16,8 +13,11 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.ArgumentMatchers.any
+import org.mockito.ArgumentMatchers.anyLong
 import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
+
 
 @RunWith(MockitoJUnitRunner::class)
 class SplashViewModelTest {
@@ -38,15 +38,22 @@ class SplashViewModelTest {
 
     @Before
     fun setUp() {
-        vm = SplashViewModel(getSignedUser, Dispatchers.Unconfined, handler)
+        whenever(handler.postDelayed(any(Runnable::class.java), anyLong())).thenAnswer {
+            it.getArgument(0,Runnable::class.java).run()
+            null
+        }
+        runBlocking {
+            whenever(getSignedUser.invoke()).thenReturn(mockedUser)
+            vm = SplashViewModel(getSignedUser, Dispatchers.Unconfined, handler)
+        }
     }
 
     @Test
     fun `check navigate to main when have user signed`() {
         runBlocking {
-            whenever(getSignedUser.invoke()).thenReturn(mockedUser)
+            vm.model.observeForever(observer)
+            verify(observer).onChanged(SplashViewModel.UiModel.NavigateToMain)
         }
-        verify(observer).onChanged(SplashViewModel.UiModel.NavigateToMain)
     }
 
     private val mockedUser = User(
